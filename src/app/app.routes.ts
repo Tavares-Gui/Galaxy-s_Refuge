ae9836a60cd3e4e5cd5a8f600a77220413658d69
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2023-11-23 16:23:15 -0300

    Finalizando menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ClientMenuComponent } from './clientMenu/clientMenu.component';
+
+export const routes: Routes = [
+    { path: 'clientMenu', component: ClientMenuComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled' })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
