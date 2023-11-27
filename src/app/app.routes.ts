84b7e9e3f8fbde7cf373c081300704f4a5248143
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2023-11-27 16:41:57 -0300

    Inicio da tela cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ClientMenuComponent } from './clientMenu/clientMenu.component';
 import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './register/register.component'
 
 export const routes: Routes = [
     { path: 'clientMenu', component: ClientMenuComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
